d4fe2aacc1cd30a1f96d9ba37b91512bd4c90ecf
Fan Ni <ni.fan@outlook.com>
2018-06-14 10:40:10 -0400

    update SQL

--- SQL/175-CombineTwoTables.sql
+++
@@ -1,1 +0,0 @@
-SELECT FirstName, LastName, City, State FROM Person AS p LEFT JOIN Address AS addr ON p.PersonId = addr.PersonId;
--- SQL/181-EmployeesEarningMoreThanTheirManagers.sql
+++
@@ -1,1 +0,0 @@
-SELECT e.Name AS Employee FROM Employee AS e JOIN Employee AS m ON e.ManagerId = m.Id WHERE e.Salary > m.Salary;
